ae4f25357ed34e1c7c1d9cf373e45ba0735217d2
chucknezi <jnezia1@gmail.com>
2020-06-14 15:17:56 -0400

    working with more structural  directives

--- basics-components/src/app/servers/servers.component.ts
+++ basics-components/src/app/servers/servers.component.ts
@@ -17,6 +17,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2']
 
 
 
@@ -31,6 +32,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
